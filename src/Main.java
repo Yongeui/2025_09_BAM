167b149cfc17d1b8f1d8885720648e4d9739adbe
Yongeui <jakeboy@naver.com>
2025-03-29 23:11:01 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,72 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Main {
-	public static void main(String[] args) {
-
+	public static void main (String [] args) {
 		System.out.println("==프로그램 시작==");
-
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		int cmd = sc.nextInt();
 		
+		Scanner sc= new Scanner (System.in);
+		int PostNumber = 0;
+		List<Article> articles = new ArrayList<Article> ();
 		
-		System.out.println("입력한 문자열 :" + str);
-		System.out.println("입력한 정수형 :" + cmd);
-		System.out.println("==프로그램 끝==");
-
+		while (true) {
+			System.out.printf("명령어 ) " );
+			String command = sc.nextLine();
+			
+			if (command.length() ==0 ) {
+				
+				System.out.println("너 명령어 입력 안했어");
+				continue;
+			}
+			
+			if (command.equals("exit")) {
+				break; //루프 탈출
+			}
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
+				System.out.println("게시글이 없습니다");
+				}
+				else {
+					System.out.println("게시글이 있습니다.");
+				}
+				
+			} else if (command.equals("article write")) {
+				int id = PostNumber + 1;
+				System.out.printf("제목: " );
+				String title = sc.nextLine();
+				System.out.printf("내용: " );
+				String body = sc.nextLine();
+				
+				System.out.println("입력된 제목 : " + title);
+				System.out.println("입력된 제목 : " + body);
+				
+				Article article = new Article (id, title, body);
+				articles.add(article);
+				
+				System.out.printf("%d 번글이 생성되었습니다.\n", id);
+				PostNumber++;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+				continue; //만나면 skip 맨위로 올라감
+			}
+		}
+		
+		System.out.println("==프로그램 종료==");
+		
+		sc.close();
 	}
-
 }
-// git init (local에서 git파일 만들기)
-// git remote add origin (origin 옆에 띄어쓰기하고 깃주소입력 [깃과 프로젝트를 연결])
-// vim .gitignore 후에 a를 눌러서 text에 입력환경 만들고
-// 입력환경이 만들어졌으면 제외할 파일명단 작성
-// .classpath, .project, .settings, bin, build 입력 후 esc 누르고 후에 :wq! 하고 enter
-// git status -> git add . -> git status -> git commit -m " " (" "안에 이름)
-// git config --global user.name "닉네임" -> git config --global user.email "이메일"
-// git config user.name, git config user.email을 통해 닉넴과 메일 확인
-// 마지막으로 깃 푸쉬. git push origin master
-//수정하고 적용할때는 git status -> git add . -> git commit -m "수정내용" -> git push origin master
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article (int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
