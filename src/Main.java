190235ea6011652cd70ec75be1db2223651f7a9b
Yongeui <jakeboy@naver.com>
2025-03-29 21:14:10 +0900

    Main.java 주석추가

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,12 @@ public class Main {
 	}
 
 }
-//git init (local에서 git파일 만들기)
-//git remote add origin (origin 옆에 띄어쓰기하고 깃주소입력 [깃과 프로젝트를 연결])
-//vim .gitignore 후에 a를 눌러서 text에 입력환경 만들고
-//입력환경이 만들어졌으면 제외할 파일명단 작성
+// git init (local에서 git파일 만들기)
+// git remote add origin (origin 옆에 띄어쓰기하고 깃주소입력 [깃과 프로젝트를 연결])
+// vim .gitignore 후에 a를 눌러서 text에 입력환경 만들고
+// 입력환경이 만들어졌으면 제외할 파일명단 작성
 // .classpath, .project, .settings, bin, build 입력 후 esc 누르고 후에 :wq! 하고 enter
+// git status -> git add . -> git status -> git commit -m " " (" "안에 이름)
+// git config --global user.name "닉네임" -> git config --global user.email "이메일"
+// git config user.name, git config user.email을 통해 닉넴과 메일 확인
+// 마지막으로 깃 푸쉬. git push origin master
